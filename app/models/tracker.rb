c74c7b3562931b8d22db0ca6f842501dd8240762
Lawrence Tran <lawrencetran1@gmail.com>
2015-05-04 20:03:42 -0700

    Removed polymorphic table and added lessons_users join table

--- app/models/tracker.rb
+++
@@ -1,3 +0,0 @@
-class Tracker < ActiveRecord::Base
-	belongs_to :trackable, polymorphic: true
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 		has_secure_password
     has_many :tutorials
+    has_many :lessons, through: :tutorials
     has_many :comments
     has_many :identities
-    has_many :trackers, as: :trackable
 end
