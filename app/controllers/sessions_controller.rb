309608ba7c6a4b4a87d9f07804b5ea0bdcf4bb72
Lawrence Tran <lawrencetran1@gmail.com>
2015-05-05 21:18:36 -0700

    Adds email login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,27 @@
 class SessionsController < ApplicationController
-	def create
+
+	def new
+		@user
+	end
+
+	def create_fb
 		user = User.from_omniauth(request.env['omniauth.auth'])
 		session[:user_id] = user.id
 		redirect_to root_url
 	end
+
+	# Method to log user in, will pass if user exists and password is correct
+	def create
+		user = User.find_by_email(params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect_to root_url
+		else
+			# if user does not exist or password is incorrect, show error message
+			flash[:alert] = "Incorrect email or password"
+			redirect_to '/login'
+		end
+	end	
 
 	def destroy
 		session[:user_id] = nil
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,44 @@
 class UsersController < ApplicationController
+	# If user is logged in, disable all methods except for new and create
+	before_action :require_user, except: [:new, :create]
 
+	# Method to create a new user
+	def new
+		@user = User.new
+	end
 
+	# Method to edit user profile
+	def edit
+		@user = User.find(params[:id])
+	end
 
+	# Method to update user profile, redirects to user profile if successful
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to current_user
+    else
+    	# Error messages if update does not pass validations
+      flash[:alert] = @user.errors.full_messages
+      render "edit"
+    end
+  end	
+
+	# Creates a new user and redirects to root if successful
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id.to_s
+			redirect_to '/'
+		else
+			# Error messages if user tries to create a new user that does not pass validations
+			flash[:alert] = @user.errors.full_messages
+			render 'new'
+		end
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+		end
 end
