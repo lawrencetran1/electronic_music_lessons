22894f8f45fb3ae75ee071d5f52ac6cfbe5c7378
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-05-06 18:08:42 -0700

    created form for tutorials

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,2 +1,62 @@
 class TutorialsController < ApplicationController
+
+
+        def index
+            @tutorials = Tutorial.all
+        end
+
+
+        def show
+            @tutorial = Tutorial.find(params[:id])
+        end
+
+
+        def new
+            @tutorial = Tutorial.new
+        end
+
+
+
+        def create
+            @tutorial = Tutorial.new(tutorials_params)
+        if @tutorial.save
+                redirect_to tutorials_path
+            else
+                render 'new'
+            end
+        end
+
+
+
+        def edit
+            @tutorial = Tutorial.find(params[:id])
+        end
+
+
+        def update
+            @tutorial = Tutorial.find(params[:id])
+
+            if @tutorial.update_attributes(params.require(:tutorial).permit(:name, :description, :category))
+                redirect_to tutorials_path
+            else
+                render "edit"
+            end
+        end
+
+
+        def destroy
+            @tutorial = Tutorial.find(params[:id])
+            @tutorial.destroy
+            redirect_to tutorials_path
+        end
+
+
+
+        private
+        def tutorial_params
+            params.require(:tutorial).permit(:name, :description, :category)
+        end
+
+
+
 end
