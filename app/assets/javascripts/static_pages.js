957a0c0d079faafe56a8339dcf9632c5719f761c
Lawrence Tran <lawrencetran1@gmail.com>
2015-05-06 18:03:56 -0700

    Adds jquery to consume api

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$.get("/api/tutorials/", function(data){
+		console.log(data)
+
+		$.each(data, function(index, item){
+			var large = '<div class="container"><div class="col s4"><div class="card medium"><div class="card-image"><img src="http://www.dvorak.org/blog/wp-content/uploads/2012/02/SoundWave.jpg"><span id="tutorial-name" class="card-title">' + item.name + '</span></div><div class="card-content"><p>' + item.description + '</p></div><div class="card-action"><a href="#">Start</a><a href="#">Like</a></div></div></div></div>';
+			$('#tutorial-section').append(large);
+		});
+	});
+});
