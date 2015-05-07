a40aacd14489693396826b96fd92078feb95d328
Emilie Corpuz <emiliecorpuz@gmail.com>
2015-05-07 14:28:51 -0700

    add tutorial photos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,17 @@
 class StaticPagesController < ApplicationController
 
     def index
+
+    @tutorials = Tutorial.all
+    # when tutorial.category == "Audio Production"
+    #   puts "audio image"
+    # when tutorial.category == "keyboards"
+    #   puts "keyboard image"
+    # else
+    #   puts "default tutorial image" 
+    # end
+
+
     end
 
     def about
