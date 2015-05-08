88a26beeca12b5226df52c427a2a86257624dbff
Lawrence Tran <lawrencetran1@gmail.com>
2015-05-08 16:38:26 -0700

    adds tutorial category image

--- app/controllers/api/tutorials_controller.rb
+++ app/controllers/api/tutorials_controller.rb
@@ -1,9 +1,14 @@
 module Api
+    include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::AssetTagHelper
     class TutorialsController < ApplicationController
     protect_from_forgery with: :null_session
 
     def index 
       tutorials = Tutorial.all
+      tutorials.each do |t|
+        t.picture = view_context.image_path t.picture 
+      end
       render json: tutorials, only: [:name, :description,:category, :picture]
 
     end
