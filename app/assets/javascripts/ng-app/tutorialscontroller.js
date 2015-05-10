5363c9cda6cdc6bbb45a6b08d75607edb41a39c4
Emilie Corpuz <emiliecorpuz@gmail.com>
2015-05-09 18:53:37 -0700

    lesson page styling, test carrierwave

--- app/assets/javascripts/ng-app/tutorialscontroller.js
+++ app/assets/javascripts/ng-app/tutorialscontroller.js
@@ -6,7 +6,7 @@
     TutorialsController.$inject = ['$http','$resource'];
 
     function TutorialsController($http, $resource){
-      var tutorial = $resource('/api/tutorials/:id',{id:'@id'});
+      // var tutorial = $resource('/api/tutorials/:id',{id:'@id'});
       // capture variable
       var self = this;
       // Create a promise - asynchronis
@@ -18,6 +18,27 @@
        promise.then(function(response){
         self.tutorials = response.data;
       });
+
+      var tutorial = $http.get('/api/tutorials/1').
+          success(function(data, status, headers, config){
+          return data;  
+        });
+
+       tutorial.then(function(response){
+        console.log(response.data);
+        self.tutorial = response.data;
+      });
+
+      var lessons = $http.get('/api/tutorials/1').
+          success(function(data, status, headers, config){
+          return data;  
+        });
+        lessons.then(function(response){
+        console.log(response.data.lessons);
+        self.lessons = response.data.lessons;
+      }); 
+
+
 
       }
 
