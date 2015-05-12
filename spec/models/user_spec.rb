24eac43c18d07ec5367003ff791f85ce6f3f5a3d
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-05-11 18:58:17 -0700

    tested first_name and last_name of the user model together with success.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-    #     it "responds to a first name" do
-    #         #Does the first name exist?
-    #     expect(subject).to respond_to(:first_name)
-    # end
+        it "responds to a first name" do
+            #Does the first name exist?
+        expect(subject).to respond_to(:first_name)
+    end
 
-    #     it "is invalid without a first name" do
-    #         #Can a user be created without a first name?
-    #     expect(subject).to be_invalid
-    # end
+        it "is invalid without a first name" do
+            #Can a user be created without a first name?
+        expect(subject).to be_invalid
+    end
 
-    #     it "raises an error without a first name" do
-    #         #Will it raise an error without a first name?
-    #     expect{subject.save!}.to raise_error(ActiveRecord::RecordInvalid)
-    # end
+        it "raises an error without a first name" do
+            #Will it raise an error without a first name?
+        expect{subject.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    end
 
 
         it "responds to a last name" do
